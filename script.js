0049ea6c8f466bb12f80b3ea4c0ac18c96d3e27f
NGR Grok <jimmybiscuit74@mail.com>
2024-09-13 21:34:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('https://api.github.com/repos/ngr-grok/NGR-grok.github.io/contents/sites')
   .then(response => response.json())
   .then(data => {
-    const fileCount = data.length-1;
+    const fileCount = data.length;
     document.getElementById('sites-hosting').textContent = `Currently hosting ${fileCount} sites.`;
   })
   .catch(error => console.error('Error fetching directory contents:', error));
